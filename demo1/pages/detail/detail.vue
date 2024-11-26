1f8560b2b9c6bc134e3a9dc08d08788b8ff7d457
WeiHsuanLai <hsuan29684@gmail.com>
2024-11-26 10:17:43 +0800

    新增API調用

--- demo1/pages/detail/detail.vue
+++ demo1/pages/detail/detail.vue
@@ -4,12 +4,12 @@
 		<view class="content">{{ objData.body }}</view>
 		<view class="comment">
 			<view class="text">評論</view>
-			<view class="row">
+			<view class="row" v-for="item in comment" :key="item.id">
 				<view class="top">
-					<view class="name">名稱</view>
-					<view class="mail">郵件</view>
+					<view class="name">{{item.name}}</view>
+					<view class="mail">{{item.email}}</view>
 				</view>
-				<view class="body">主要內容</view>
+				<view class="body">{{item.body}}</view>
 			</view>
 		</view>
 	</view>
@@ -21,29 +21,58 @@
 			return {
 				objData: {}, // 用於存儲 API 獲取的數據
 				id: 1,
+				comment: []
 			};
 		},
 		onLoad(e) {
 			this.id = e.id
 			// 生命周期：頁面加載時調用
 			this.getDetail(); // 調用方法獲取數據
+			this.getComment()
 		},
 		methods: {
 			getDetail() {
+				uni.showLoading({
+					title: "數據加載中...",
+					mask: true
+				})
 				uni.request({
 					url: "https://jsonplaceholder.typicode.com/posts/" + this.id, // API 地址
 					success: (res) => {
 						console.log(res);
 						this.objData = res.data; // 更新數據
 					},
+					complete: () => {
+						uni.hideLoading()
+					},
 					fail: (err) => {
 						uni.showToast({
 							title: "加載失敗123",
 							icon: "none",
 						});
+						uni.hideLoading()
 					},
 				});
 			},
+			getComment() {
+				uni.request({
+					url: `https://jsonplaceholder.typicode.com/posts/${this.id}/comments`,
+					success: (res) => {
+						console.log(res);
+						this.comment = res.data; // 更新數據
+					},
+					complete: () => {
+						uni.hideLoading()
+					},
+					fail: (err) => {
+						uni.showToast({
+							title: "加載失敗123",
+							icon: "none",
+						});
+						uni.hideLoading()
+					},
+				})
+			}
 		},
 	};
 </script>
@@ -66,7 +95,7 @@
 
 	.comment {
 		padding: 30rpx;
-		background: #666;
+		background: #f4f4f4;
 
 		.text {
 			font-size: 46rpx;
@@ -74,9 +103,15 @@
 		}
 
 		.row {
+			border-bottom: 1px solid #e8e8e8;
+			padding: 20rpx 0;
+
 			.top {
 				display: flex;
 				justify-content: space-between;
+				font-size: 22rpx;
+				color: #999;
+				padding-bottom: 15rpx;
 			}
 		}
 	}
